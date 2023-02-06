3a84eeca9149290e26e542072716c6726d446c60
kuppambhavya <112753722+kuppambhavya@users.noreply.github.com>
2023-02-06 08:13:57 +0530

    Add files via upload

---
+++ map.py
@@ -0,0 +1,42 @@
+MAX, MIN = 1000, -1000
+def minimax(depth, nodeIndex, maximizingPlayer,
+			values, alpha, beta):
+
+	if depth == 3:
+		return values[nodeIndex]
+
+	if maximizingPlayer:
+	
+		best = MIN
+
+		for i in range(0, 2):
+			
+			val = minimax(depth + 1, nodeIndex * 2 + i,
+						False, values, alpha, beta)
+			best = max(best, val)
+			alpha = max(alpha, best)
+
+			if beta <= alpha:
+				break
+		
+		return best
+	
+	else:
+		best = MAX
+
+		for i in range(0, 2):
+		
+			val = minimax(depth + 1, nodeIndex * 2 + i,
+							True, values, alpha, beta)
+			best = min(best, val)
+			beta = min(beta, best)
+
+			if beta <= alpha:
+				break
+		
+		return best
+	
+if __name__ == "__main__":
+
+	values = [2,3,5,9,0,1,7,5]
+	print("The optimal value is :", minimax(0, 0, True, values, MIN, MAX))
